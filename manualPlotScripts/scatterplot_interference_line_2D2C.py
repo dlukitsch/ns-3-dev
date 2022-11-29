e8b4067c5bd3ec25af0487786fcbc11e3645057f
Daniel Lukitsch <lukitsch.daniel96@gmail.com>
2022-11-29 01:16:18 +0100

    Add last plotting scripts

--- manualPlotScripts/scatterplot_interference_line_2D2C.py
+++ manualPlotScripts/scatterplot_interference_line_2D2C.py
@@ -5,8 +5,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set_theme(style="dark")
 
-sim_data = pd.read_csv("../output/Square_5x5_Interference_5ms_Mpl_1D0C/results/results_1.csv")
-pos_data = pd.read_csv("../examples/multicast/Square_5x5_Interference.csv")
+sim_data = pd.read_csv("../output/Line_10_Interference_5ms_Mpl_2D2C/results/results_1.csv")
+pos_data = pd.read_csv("../examples/multicast/Line_10_Interference.csv")
 
 sim_data['x'] = pos_data['x']
 sim_data['y'] = pos_data['y']
@@ -15,8 +15,9 @@ sim_data['# Sent Packets'] = sim_data['Successfully Sent Data-Packets']
 sim_data['PDR [%]'] = round(sim_data['Different Received Data Packets'] / 60 * 100, 1)
 sim_data['PDR [%]'][0] = 100
 
-inter_x = [ 165, 275]
-inter_y = [ 220, 220]
+
+inter_x = [ 495, 605]
+inter_y = [ 0, 0]
 
 #cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
 sns.set(style="ticks")
@@ -25,20 +26,20 @@ pal = sns.dark_palette("#69d", reverse=True, as_cmap=True)
 
 #g = sns.FacetGrid(pos_data, col="x", row="Successfully Sent Packets")
 #f = sns.relplot(x="x", y="y", size="Successfully Sent Bytes", sizes=(100, 1000), data=sim_data)
-ax1 = sns.scatterplot(data=sim_data, x ='x', y = 'y', hue = "PDR [%]", size = "# Sent Packets", sizes=(0,250), hue_norm = (0, 100), size_norm = (40, 60), legend=True, linewidth=0, palette =pal)
+ax1 = sns.scatterplot(data=sim_data, x ='x', y = 'y', hue = "PDR [%]", size = "# Sent Packets", sizes=(50,500), hue_norm = (0, 100), size_norm = (0, 60), legend=True, linewidth=0, palette =pal)
 sns.scatterplot(x=inter_x, y = inter_y, legend=False, linewidth=1,  palette = "orange" )
 sns.scatterplot(x=inter_x, y = inter_y, legend=False, facecolors='orange', edgecolor='orange', size=(110))
 sns.scatterplot(x=inter_x, y = inter_y, legend=False, s = 22000 ,facecolors='none',  linewidth=1, edgecolor="red" )
 
 #ax1.ax_joint.plot([0],[0],'o',ms=60 , mec='r', mfc='none')
 
-ax1.set(xlabel='X-Coordinate [m]', ylabel='Y-Coordinate [m]', title= "MPL 1D0C with Interference")
+ax1.set(xlabel='X-Coordinate [m]', ylabel='Y-Coordinate [m]', title= "MPL 2D2C with Interference")
 ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-ax1.set_xlim((-50, 490))
-ax1.set_ylim((-50, 490))
+ax1.set_xlim((-50, 1030))
+ax1.set_ylim((-20, 20))
 
 ax1.set_xticks((sim_data['x']))
 ax1.set_yticks((sim_data['y']))
 
-ax1.figure.savefig("scatterplot_Intererence_Square_1D0C.pdf", bbox_inches='tight', dpi=300)
+ax1.figure.savefig("scatterplot_Intererence_Line_2D2C.pdf", bbox_inches='tight', dpi=300)
--- manualPlotScripts/scatterplot_interference_square_2D1C.py
+++ manualPlotScripts/scatterplot_interference_square_2D1C.py
@@ -5,22 +5,18 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 sns.set_theme(style="dark")
 
-sim_data = pd.read_csv("../output/Square_5x5_Interference_5ms_Mpl_2D1C/results_mean.csv")
+sim_data = pd.read_csv("../output/Square_5x5_Interference_5ms_Mpl_2D1C/results/results_1.csv")
 pos_data = pd.read_csv("../examples/multicast/Square_5x5_Interference.csv")
 
 sim_data['x'] = pos_data['x']
 sim_data['y'] = pos_data['y']
 
 sim_data['# Sent Packets'] = sim_data['Successfully Sent Data-Packets']
-print(sim_data['# Sent Packets'][0])
-sim_data['PDR [%]'] = sim_data['Successfully Received Data-Packets'] / sim_data['# Sent Packets'][0] * 100
-print(sim_data['PDR [%]'])
+sim_data['PDR [%]'] = round(sim_data['Different Received Data Packets'] / 60 * 100, 1)
+sim_data['PDR [%]'][0] = 100
 
 inter_x = [ 165, 275]
 inter_y = [ 220, 220]
-
-for col in sim_data.columns:
-    print(col)
 
 #cmap = sns.cubehelix_palette(rot=-.2, as_cmap=True)
 sns.set(style="ticks")
@@ -30,12 +26,9 @@ colors = ["#242629"] #['#6699dd', '#5982b9', '#4c6b95', '#3e5471', '#313d4d', '#
 pal_or = sns.dark_palette("#69d", reverse=True, as_cmap=True)
 pal = sns.set_palette(colors)
 
-
-fig = plt.figure()
-
 #g = sns.FacetGrid(pos_data, col="x", row="Successfully Sent Packets")
 #f = sns.relplot(x="x", y="y", size="Successfully Sent Bytes", sizes=(100, 1000), data=sim_data)
-ax1 = sns.scatterplot(data=sim_data, x ='x', y = 'y', hue = "PDR [%]", size = "# Sent Packets", sizes=(50,500), legend=True, hue_norm = (0, 500), size_norm = (40, 120) ,linewidth=0, palette =pal_or)
+ax1 = sns.scatterplot(data=sim_data, x ='x', y = 'y', hue = "PDR [%]", size = "# Sent Packets", sizes=(50,500), hue_norm = (0, 100), size_norm = (0, 250), legend=True, linewidth=0, palette =pal)
 sns.scatterplot(x=inter_x, y = inter_y, legend=False, linewidth=1,  edgecolor="orange", facecolors="orange",  size=(110) )
 sns.scatterplot(x=inter_x, y = inter_y, legend=False, s = 22000 ,facecolors='none',  linewidth=1, edgecolor="red" )
 
@@ -44,16 +37,10 @@ sns.scatterplot(x=inter_x, y = inter_y, legend=False, s = 22000 ,facecolors='non
 ax1.set(xlabel='X-Coordinate [m]', ylabel='Y-Coordinate [m]', title= "MPL 2D1C with Interference")
 ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
-
 ax1.set_xlim((-50, 490))
 ax1.set_ylim((-50, 490))
 
 ax1.set_xticks((sim_data['x']))
 ax1.set_yticks((sim_data['y']))
 
-plt.tight_layout()
-plt.plot()
-
-#g.savefig("FacetGrid.pdf", bbox_inches='tight', dpi = 300)
-#f.savefig("relplot.pdf",  bbox_inches='tight', dpi=300)
-ax1.figure.savefig("scatterplot_Intererence_Square_2D1C.svg", dpi=1200)
+ax1.figure.savefig("scatterplot_Intererence_Square_2D1C.pdf", bbox_inches='tight', dpi=300)
